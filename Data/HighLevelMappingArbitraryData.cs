ae4d01d11dda81267f341b778b06d2650be5972c
cory hain <sullenchimp@gmail.com>
2023-03-12 14:57:19 -0700

    added data and tests project

---
+++ Data/HighLevelMappingArbitraryData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.DocumentModel;
+using Amazon.Runtime;
+using Amazon.SecurityToken;
+
+namespace Data
+{
+    class HighLevelMappingArbitraryData
+    {
+    }
+}
---
+++ Data/entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.entities
+{
+    internal class User
+    {
+        public string Id { get; set; }       
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string UserId { get; set; }
+        public string UserName { get; set; }
+
+
+    }
+}
